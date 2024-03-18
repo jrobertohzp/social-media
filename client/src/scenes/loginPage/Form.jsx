f67268901f6d816c417423d34645a3a5ae202e9d
Rob <jrobertohzp@gmail.com>
2024-03-18 01:31:44 -0600

    Login Form completed

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -24,12 +24,12 @@ const registerSchema = yup.object().shape({
     location: yup.string().required("Location is required"),
     ocupation: yup.string().required("Ocupation is required"),
     picture: yup.string().required("Picture is required")
-})
+});
 
 const loginSchema = yup.object().shape({
     email: yup.string().email("Invalid email").required("Email is required"),
     password: yup.string().required("Password is required"),
-})
+});
 
 const initialValuesRegister = {
     firstName: "",
@@ -38,24 +38,71 @@ const initialValuesRegister = {
     password: "",
     location: "",
     ocupation: "",
-    picture: ""
-}
+    picture: "",
+};
 
 const initialValuesLogin = {
     email: "",
-    password: ""
-}
+    password: "",
+};
 
 const Form = () => {
     const [pageType, setPageType] = useState("login");
     const { palette } = useTheme();
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const isNonMobile = useMediaQuery("(min-width: 600px)");
+    const isNonMobile = useMediaQuery("(min-width:600px)");
     const isLogin = pageType === "login";
     const isRegister = pageType === "register";
 
-    const handleFormSubmit = async (values, onSubmitProps) => { };
+    const register = async (values, onSubmitProps) => {
+        // this allows us to send form info with image
+        const formData = new FormData();
+        for (let value in values) {
+            formData.append(value, values[value]);
+        }
+        formData.append("picturePath", values.picture.name);
+
+        const savedUserResponse = await fetch(
+            "http://localhost:3001/auth/register",
+            {
+                method: "POST",
+                body: formData,
+            }
+        );
+        const savedUser = await savedUserResponse.json();
+        onSubmitProps.resetForm();
+
+        if (savedUser) {
+            setPageType("login");
+        }
+    };
+    const login = async (values, onSubmitProps) => {
+        const loggedInResponse = await fetch(
+            "http://localhost:3001/auth/login",
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(values),
+            });
+        const loggedIn = await loggedInResponse.json();
+        onSubmitProps.resetForm();
+        if (loggedIn) {
+            dispatch(
+                setLogin({
+                    user: loggedIn.user,
+                    token: loggedIn.token,
+                })
+            );
+            navigate("/home")
+        }
+    };
+
+
+    const handleFormSubmit = async (values, onSubmitProps) => {
+        if (isLogin) await login(values, onSubmitProps);
+        if (isRegister) await register(values, onSubmitProps);
+    };
 
     return (
         <Formik
@@ -71,8 +118,7 @@ const Form = () => {
                 handleChange,
                 handleSubmit,
                 setFieldValue,
-                resetForm
-
+                resetForm,
             }) => (
                 <form onSubmit={handleSubmit}>
                     <Box
@@ -146,7 +192,7 @@ const Form = () => {
                                                 sx={{ "&:hover": { cursor: "pointer" } }}
                                             >
                                                 <input {...getInputProps()} />
-                                                {values.picture ? (
+                                                {!values.picture ? (
                                                     <p>Add Picture Here</p>
                                                 ) : (
                                                     <FlexBetween>
@@ -160,16 +206,67 @@ const Form = () => {
                                 </Box>
                             </>
                         )}
+
+                        <TextField
+                            label="Email"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.email}
+                            name="email"
+                            error={Boolean(touched.email) && Boolean(errors.email)}
+                            helperText={touched.email && errors.email}
+                            sx={{ gridColumn: "span 4" }}
+                        />
+                        <TextField
+                            label="Password"
+                            type="password"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.password}
+                            name="password"
+                            error={Boolean(touched.password) && Boolean(errors.password)}
+                            helperText={touched.password && errors.password}
+                            sx={{ gridColumn: "span 4" }}
+                        />
+                    </Box>
+
+                    {/* BUTTONS */}
+                    <Box>
+                        <Button
+                            fullWidth
+                            type="submit"
+                            sx={{
+                                m: "2rem 0",
+                                p: "1rem",
+                                backgroundColor: palette.primary.main,
+                                color: palette.background.alt,
+                                "&:hover": { color: palette.primary.main },
+                            }}
+                        >
+                            {isLogin ? "LOGIN" : "REGISTER"}
+                        </Button>
+                        <Typography
+                            onClick={() => {
+                                setPageType(isLogin ? "register" : "login");
+                                resetForm();
+                            }}
+                            sx={{
+                                testDecoration: "underline",
+                                color: palette.primary.main,
+                                "&:hover": {
+                                    cursor: "pointer",
+                                    color: palette.primary.light
+                                },
+                            }}
+                        >
+                            {isLogin
+                                ? "Don't have an account? Sign Up here"
+                                : "Already have an account? Login here"}
+                        </Typography>
                     </Box>
                 </form>
-            )
-            }
+            )}
         </Formik >
-
-    )
-
-
-}
-
-
+    );
+};
 export default Form; 
